2b23ab4068d0dabed31d46ab3b311e6bfc9699b0
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-08-22 19:30:44 -0600

    inicio de proyecto anylist

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'; //importante para usar apollo sandbox 2025
+import { join } from 'path';
+import { ItemsModule } from './items/items.module';
+
+
+@Module({
+  imports: [
+  GraphQLModule.forRoot<ApolloDriverConfig>({
+    driver: ApolloDriver,
+     // debug: false,
+       playground: false,
+       autoSchemaFile: join( process.cwd(), 'src/schema.gql'),
+       plugins: [
+        ApolloServerPluginLandingPageLocalDefault()
+       ],
+           }),
+  ItemsModule,
+     ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
