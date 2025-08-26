5ae4b7ceab0826ac48f1605f0d28004b21fcb3d8
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-08-26 12:25:15 -0600

    creacion de funcion en user.service para el manejo de errores de signup

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
 import { User } from './entities/user.entity';
@@ -9,6 +9,8 @@ import { Repository } from 'typeorm';
 @Injectable()
 
 export class UsersService {
+
+  private logger = new Logger('UsersService');
 
   constructor(
     @InjectRepository(User)
@@ -22,8 +24,7 @@ export class UsersService {
       return await this.userRepository.save(newUser);
 
     } catch(error){
-      console.log(error);
-      throw new BadRequestException('Error creating user - Check server logs' );
+      this.handleDBErrors(error);
     }
   }
 
@@ -41,5 +42,15 @@ export class UsersService {
 
   block(id: string): Promise<User> {
     throw new Error(`Method not implemented. ID: ${id}`);
+  }
+
+  private handleDBErrors(error: any ): never{
+
+    if( error.code == '23505'){
+      throw new BadRequestException(error.detail.replace('key',''));
+    }
+    this.logger.error(error);
+
+    throw new InternalServerErrorException('Unexpected error, please check logs');
   }
 }
