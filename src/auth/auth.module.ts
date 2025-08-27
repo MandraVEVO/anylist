43b96399fdd2e74d951eb18f6680aac23c77dfbc
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-08-26 18:44:42 -0600

    Configuracion de passport module y JWT Module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,35 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { retry } from 'rxjs';
 
 @Module({
   providers: [AuthResolver, AuthService],
   imports: [
-    UsersModule
+    UsersModule,
+
+    ConfigModule,
+
+    PassportModule.register({defaultStrategy:'jwt'}),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+
+        console.log(configService.get('JWT_SECRET'));
+
+        return{
+          secret: configService.get('JWT_SECRET'),
+          signOptions:{
+            expiresIn: '4h'
+          }
+        }
+      }
+    })
   ],
 })
 export class AuthModule {}
