3c9b88d6907c32bd409ceabd8026e71a6845bfc9
MandraVEVO <paratrabajosquesenecesiten15@gmail.com>
2025-08-28 12:50:16 -0600

    Autorizacion de usuarios - roles

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -1,10 +1,11 @@
-import { createParamDecorator, ExecutionContext, InternalServerErrorException } from '@nestjs/common';
+import { createParamDecorator, ExecutionContext, ForbiddenException, InternalServerErrorException } from '@nestjs/common';
 import { GqlArgumentsHost, GqlExecutionContext } from '@nestjs/graphql';
 import { retry } from 'rxjs';
+import { ValidRoles } from '../enums/valid-roles.enum';
 
 
 export const CurrentUser = createParamDecorator(
-    (roles = [], context : ExecutionContext)=>{
+    (roles: ValidRoles[] = [], context: ExecutionContext) => {
 
         const ctx = GqlExecutionContext.create(context);
         const user = ctx.getContext().req.user;
@@ -12,6 +13,15 @@ export const CurrentUser = createParamDecorator(
         if(!user){
             throw new InternalServerErrorException('No user inside the request - make sure that we used AuthGuard')
         }
+        if(roles.length === 0) return user;
 
-        return user;
+        for (const role of user.roles){
+            //TODO: ELIMINAR VALID ROLES
+            if(roles.includes(role)){
+                return user;
+            }
+        }
+            
+
+        throw new ForbiddenException(`User ${user.username} does not have the necessary roles`);
 })
